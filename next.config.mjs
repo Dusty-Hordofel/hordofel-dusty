61df71d973b7607e34e9b7982716384fc2d88206
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2024-11-30 03:11:25 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "www.nike.com", "asset.cloudinary.com"],
+    domains: [
+      "res.cloudinary.com",
+      "www.nike.com",
+      "asset.cloudinary.com",
+      "images.unsplash.com",
+    ],
   },
 };
 
