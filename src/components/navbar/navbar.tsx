bdb1ccd82cba93fec871fb4a6f16b218e367ee7a
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2024-11-30 02:45:54 +0100

    Update navbar.tsx

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -7,7 +7,7 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   const navItems = [
-    { label: "My projets", url: "/" },
+    { label: "Read.cv", url: "https://read.cv/hordofel" },
     {
       label: "LinkedIn",
       url: "https://www.linkedin.com/in/dusty-hordofel-bamana-08389310a/",
@@ -22,9 +22,11 @@ const Navbar = (props: Props) => {
             initial={{ opacity: 0, translateY: -20 }}
             animate={{ opacity: 1, translateY: 0 }}
             transition={{ duration: 0.5, delay: index * 0.3, ease: "easeOut" }}
-            className="text-black"
+            className="relative inline-block text-black after:content-[''] after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 hover:after:w-full"
           >
-            <Link href={item.url}>{item.label}</Link>
+            <Link href={item.url} target="_blank">
+              {item.label}
+            </Link>
           </motion.li>
         ))}
       </ul>
