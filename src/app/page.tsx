4952cf84b0e4ee2fb18f0b61e82028e25a8330f5
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2024-11-30 02:54:00 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { FollowerPointerCard } from "@/components/ui/following-pointer";
 import Image from "next/image";
 import { portfolioData } from "@/data/portfolio-data";
 import { ProjectCard } from "@/components/ui/project-card";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -74,6 +75,22 @@ export default function Home() {
           </p>
         </section>
       </main>
+
+      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center border-t">
+        <p className="text-sm">
+          © {new Date().getFullYear()} BAMANA Dusty Hordofel.
+          {/* Tous droits
+          réservés. */}
+        </p>
+        {/* <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <Link className="text-xs hover:underline underline-offset-4" href="#">
+            Mentions légales
+          </Link>
+          <Link className="text-xs hover:underline underline-offset-4" href="#">
+            Politique de confidentialité
+          </Link>
+        </nav> */}
+      </footer>
     </div>
   );
 }
